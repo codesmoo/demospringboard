8caebf364880409ff9e7aff0144cfe7c57e7a045
codesmoo <52499305+codesmoo@users.noreply.github.com>
2019-07-20 22:12:49 +0900

    first commit

---
+++ src/main/java/me/codesmoo/demospringboard/DemospringboardApplication.java
@@ -0,0 +1,13 @@
+package me.codesmoo.demospringboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemospringboardApplication.class, args);
+    }
+
+}
