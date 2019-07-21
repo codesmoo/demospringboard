93ea76e0d76d2ac36a2f1f15fb311a7d5be2dd28
codesmoo <52499305+codesmoo@users.noreply.github.com>
2019-07-21 22:21:48 +0900

    handlebars 이용해서 view 띄워보기

--- src/main/java/me/codesmoo/demospringboard/web/WebRestController.java
+++ src/main/java/me/codesmoo/demospringboard/web/WebRestController.java
@@ -2,15 +2,21 @@ package me.codesmoo.demospringboard.web;
 
 import me.codesmoo.demospringboard.domain.posts.PostsRepository;
 import me.codesmoo.demospringboard.domain.posts.PostsRequestDTO;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class WebRestController {
 
     private PostsRepository postsRepository;
 
     public WebRestController(PostsRepository postsRepository) {
         this.postsRepository = postsRepository;
+    }
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
     }
 
     @GetMapping("/hello")
