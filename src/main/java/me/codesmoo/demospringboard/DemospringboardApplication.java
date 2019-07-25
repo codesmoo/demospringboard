8c41a2bea8c43a246346462da7fc1c032291cb55
codesmoo <52499305+codesmoo@users.noreply.github.com>
2019-07-25 16:21:44 +0900

    외부 설정파일 받을수있도록 수정

--- src/main/java/me/codesmoo/demospringboard/DemospringboardApplication.java
+++ src/main/java/me/codesmoo/demospringboard/DemospringboardApplication.java
@@ -2,14 +2,21 @@ package me.codesmoo.demospringboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class DemospringboardApplication {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/demospringboard/real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(DemospringboardApplication.class, args);
+        new SpringApplicationBuilder(DemospringboardApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
